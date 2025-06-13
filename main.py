6f4121181e6ea54ceff36c7546bc0b9145b4ff10
2445868686 <2445868686@qq.com>
2025-06-14 00:36:01 +0800

    add file

--- main.py
+++ main.py
@@ -5,6 +5,11 @@
 """
 
 import os, sys, json, time, tempfile, platform, requests, concurrent.futures
+# 1. 获取脚本所在目录（备用）
+script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+config_dir = os.path.join(script_path, 'config')
+settings_file = os.path.join(config_dir, 'translator_settings.json')
+
 
 try:
     import DaVinciResolveScript as dvr_script
@@ -42,6 +47,11 @@ win = dispatcher.AddWindow(
         "WindowTitle": '字幕翻译',
         "Geometry": [700, 300, 400, 480],
         "Spacing": 10,
+        "StyleSheet": """
+        * {
+            font-size: 14px; /* 全局字体大小 */
+        }
+        """
     },
     [
         ui.VGroup(
@@ -56,14 +66,42 @@ win = dispatcher.AddWindow(
                                 ui.HGroup(
                                     {"Weight": 0.1},
                                     [
-                                        ui.Label({"ID": 'ModelLabel', "Text": '模型', "Alignment": {"AlignRight": False}, "Weight": 0.2}),
-                                        ui.ComboBox({"ID": 'ModelCombo', "Text": '', "Weight": 0.8}),
+                                        ui.Label({"ID": 'TrackLabel', "Text": '翻译轨道', "Alignment": {"AlignRight": False}, "Weight": 0.2}),
+                                        
+                                    ]
+                                ),
+                                ui.HGroup(
+                                    {"Weight": 0.1},
+                                    [
+                                        
+                                        ui.ComboBox({"ID": 'TrackCombo', "Text": '', "Weight": 0.8}),
+                                    ]
+                                ),
+                                ui.HGroup(
+                                    {"Weight": 0.1},
+                                    [
+                                        ui.Label({"ID": 'OpenAIModelLabel', "Text": '模型', "Alignment": {"AlignRight": False}, "Weight": 0.2}),
+                                        
+                                    ]
+                                ),
+                                ui.HGroup(
+                                    {"Weight": 0.1},
+                                    [
+                        
+                                        ui.ComboBox({"ID": 'OpenAIModelCombo', "Text": '', "Weight": 0.8}),
                                     ]
                                 ),
                                 ui.HGroup(
                                     {"Weight": 0.1},
                                     [
                                         ui.Label({"ID": 'TargetLangLabel', "Text": '目标语言', "Alignment": {"AlignRight": False}, "Weight": 0.2}),
+                                        
+                                    ]
+                                ),
+                                ui.HGroup(
+                                    {"Weight": 0.1},
+                                    [
+                                
                                         ui.ComboBox({"ID": 'TargetLangCombo', "Text": '', "Weight": 0.8}),
                                     ]
                                 ),
@@ -86,9 +124,15 @@ win = dispatcher.AddWindow(
                                 
                                 ]
                                 ),
+                                ui.HGroup({"Weight": 1}, [
+                                ui.CheckBox({"ID": "LangEnCheckBox", "Text": "EN", "Checked": True, "Alignment": {"AlignRight": True}, "Weight": 0}),
+                                ui.CheckBox({"ID": "LangCnCheckBox", "Text": "简体中文", "Checked": False, "Alignment": {"AlignRight": True}, "Weight": 1}),
+                            
+                        ]),
                             ]
                             
-                        )
+                        ),
+                        
                     ]
 
                 )
@@ -135,14 +179,161 @@ openai_config_window = dispatcher.AddWindow(
         )
     ]
 )
-itm = win.GetItems()
-itm["MyStack"].CurrentIndex = 0
-itm["MyTabs"].AddTab("翻译")
-itm["MyTabs"].AddTab("配置")
+translations = {
+    "cn": {
+        "Tabs": ["翻译","配置"],
+        "OpenAIModelLabel":"模型：",
+        "TrackLabel":"翻译轨道：",
+        "TargetLangLabel":"目标语音：",
+        "ShowOpenAI": "配置",
+        "OpenAILabel":"填写OpenAI API信息",
+        "OpenAIBaseURLLabel":"Base URL",
+        "OpenAIApiKeyLabel":"密钥",
+        "OpenAIConfirm":"确定",
+        "OpenAIRegisterButton":"注册",
+        
+    },
+
+    "en": {
+        "Tabs": ["Translator", "Configuration"],
+        "OpenAIModelLabel":"Model:",
+        "TrackLabel":"Translate Track:",
+        "TargetLangLabel":"Target Language:",
+        "ShowOpenAI": "Config",
+        "OpenAILabel":"OpenAI API",
+        "OpenAIBaseURLLabel":"Base URL",
+        "OpenAIApiKeyLabel":"Key",
+        "OpenAIConfirm":"OK",
+        "OpenAIRegisterButton":"Register",
+        
+    }
+}    
+items = win.GetItems()
+openai_items = openai_config_window.GetItems()
+items["MyStack"].CurrentIndex = 0
+
+for tab_name in translations["cn"]["Tabs"]:
+    items["MyTabs"].AddTab(tab_name)
+    
 def on_my_tabs_current_changed(ev):
-    itm["MyStack"].CurrentIndex = ev["Index"]
+    items["MyStack"].CurrentIndex = ev["Index"]
 win.On.MyTabs.CurrentChanged = on_my_tabs_current_changed
 
+track_counts  = ["1","2","3","4","5"]
+for track in track_counts:
+    items["TrackCombo"].AddItem(track)
+
+openai_models = ["gpt-4o-mini","gpt-4o-mini","gpt-4.1-nano","gpt-4.1",]
+for model in openai_models:
+    items["OpenAIModelCombo"].AddItem(model)
+
+target_language = [
+    "中文（普通话）", "中文（粤语）", "English", "Japanese", "Korean",
+    "Spanish", "Portuguese", "French", "Indonesian", "German", "Russian",
+    "Italian", "Arabic", "Turkish", "Ukrainian", "Vietnamese", "Dutch"
+]
+
+for lang in target_language:
+    items["TargetLangCombo"].AddItem(lang)  
+
+
+def check_or_create_file(file_path):
+    if os.path.exists(file_path):
+        pass
+    else:
+        try:
+            with open(file_path, 'w') as file:
+                json.dump({}, file)  
+        except IOError:
+            raise Exception(f"Cannot create file: {file_path}")
+        
+def save_settings(settings, settings_file):
+    with open(settings_file, 'w') as file:
+        content = json.dumps(settings, indent=4)
+        file.write(content)
+        
+def load_settings(settings_file):
+    if os.path.exists(settings_file):
+        with open(settings_file, 'r') as file:
+            content = file.read()
+            if content:
+                try:
+                    settings = json.loads(content)
+                    return settings
+                except json.JSONDecodeError as err:
+                    print('Error decoding settings:', err)
+                    return None
+    return None
+
+default_settings = {
+    "OpenAI_API_KEY": "",
+    "OpenAI_BASE_URL": "",
+    "OpenAI_Model": 0,
+    "OpenAI_Voice": 0,
+    "CN":True,
+    "EN":False,
+}
+
+check_or_create_file(settings_file)
+saved_settings = load_settings(settings_file) 
+
+def close_and_save(settings_file):
+    settings = {
+
+        "CN":items["LangCnCheckBox"].Checked,
+        "EN":items["LangEnCheckBox"].Checked,
+        
+        "OpenAI_API_KEY": openai_items["OpenAIApiKey"].Text,
+        "OpenAI_BASE_URL": openai_items["OpenAIBaseURL"].Text,
+        "OpenAI_Model": items["OpenAIModelCombo"].CurrentIndex,
+
+        
+    }
+
+    save_settings(settings, settings_file)
+def switch_language(lang):
+    """
+    根据 lang (可取 'cn' 或 'en') 切换所有控件的文本
+    """
+    if "MyTabs" in items:
+        for index, new_name in enumerate(translations[lang]["Tabs"]):
+            items["MyTabs"].SetTabText(index, new_name)
+
+    for item_id, text_value in translations[lang].items():
+        # 确保 items[item_id] 存在，否则会报 KeyError
+        if item_id == "Tabs":
+            continue
+        if item_id in items:
+            items[item_id].Text = text_value
+        elif item_id in openai_items:    
+            openai_items[item_id].Text = text_value
+        else:
+            print(f"[Warning] items 中不存在 ID 为 {item_id} 的控件，无法设置文本！")
+
+    # 缓存复选框状态
+    checked = items["LangEnCheckBox"].Checked
+
+
+
+def on_cn_checkbox_clicked(ev):
+    items["LangEnCheckBox"].Checked = not items["LangCnCheckBox"].Checked
+    if items["LangEnCheckBox"].Checked:
+        switch_language("en")
+        print("en")
+    else:
+        print("cn")
+        switch_language("cn")
+win.On.LangCnCheckBox.Clicked = on_cn_checkbox_clicked
+
+def on_en_checkbox_clicked(ev):
+    items["LangCnCheckBox"].Checked = not items["LangEnCheckBox"].Checked
+    if items["LangEnCheckBox"].Checked:
+        switch_language("en")
+        print("en")
+    else:
+        print("cn")
+        switch_language("cn")
+win.On.LangEnCheckBox.Clicked = on_en_checkbox_clicked
 def on_openai_close(ev):
     print("OpenAI API 配置完成")
     openai_config_window.Hide()
@@ -157,7 +348,7 @@ OPENAI_API_KEY = "sk-wLP8n2FczZrYukonSvbozSba4HyV4cBHstEPDACv8aeI6QFH"
 OPENAI_API_URL = "https://yunwu.ai/"
 OPENAI_MODEL   = "gpt-4o"
 TARGET_LANG    = "English"       # 目标语言
-CONCURRENCY    = 6               # 并发线程数（5~10 较稳）
+CONCURRENCY    = 10               # 并发线程数（5~10 较稳）
 MAX_RETRY      = 3               # 单行最多重试次数
 TIMEOUT        = 30              # 单次请求超时（秒）
 # ===========================================
@@ -285,6 +476,7 @@ def main():
          print("❌ SRT 导入失败")
 
 def on_close(ev):
+    close_and_save(settings_file)
     dispatcher.ExitLoop()
 win.On.MyWin.Close = on_close
 
